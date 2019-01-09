0a7176549dfceebe661b4a7189278463694b5f70
Norbert Korczyk <korczyk.norbert@gmail.com>
2019-01-09 11:56:39 +0100

    Dashboard Component & editorconfig configuration

--- src/components/layout/SignedOutLinks.js
+++ src/components/layout/SignedOutLinks.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const SignedOutLinks = () => {
-    return (
-        <ul className="right">
-            <li><NavLink to='/'>Signup</NavLink></li>
-            <li><NavLink to='/'>Login</NavLink></li>
-        </ul>
-    )
+  return (
+    <ul className="right">
+      <li><NavLink to='/'>Signup</NavLink></li>
+      <li><NavLink to='/'>Login</NavLink></li>
+    </ul>
+  )
 };
 
 export default SignedOutLinks;
