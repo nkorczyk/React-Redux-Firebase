9fd764a0b2166c2fd1f8d3418f726d2c95f09b4f
Norbert Korczyk <korczyk.norbert@gmail.com>
2019-01-10 11:18:43 +0100

    Adding Data to Firestore

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,9 +1,25 @@
 export const createProject = (project) => {
   return (dispatch, getState, { getFirebase, getFirestore }) => {
     // make async call to database
-    dispatch({
-      type: 'CREATE_PROJECT',
-      project
-    });
+    const firestore = getFirestore(); // reference to firestore db
+
+    // reference to projects collection
+    firestore.collection('projects').add({
+      ...project,
+      authorFirstName: 'Net',
+      authorLastName: 'Ninja',
+      authorId: 12345,
+      createdAt: new Date()
+    }).then(() => {
+      dispatch({
+        type: 'CREATE_PROJECT',
+        project
+      });
+    }).catch((error) => {
+      dispatch({
+        type: 'CREATE_PROJECT_ERROR',
+        error
+      })
+    })
   }
 };
